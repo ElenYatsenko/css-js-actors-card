c2de95902684daac5f858f881ac5a8cdc5b9ef4f
Elen <15el.shev@gmail.com>
2022-06-16 22:18:35 +0300

    Add function with lessons

---
+++ assets/js/index.js
@@ -0,0 +1,136 @@
+"use strict";
+const root = document.getElementById("root");
+
+const mapSocialClass = new Map();
+mapSocialClass.set("www.facebook.com", "fa-facebook");
+mapSocialClass.set("www.twitter.com", "twitter");
+mapSocialClass.set("www.instagram.com", "instagram");
+
+function fullName(actor) {
+  return `${actor.firstName} ${actor.lastName}`;
+}
+
+/**
+ *
+ * @param {string} tag
+ * @param {object} options
+ * @param {string[]} options.classNames
+ * @param {objects} children
+ * @returns
+ */
+function createElement(
+  tag = "div",
+  { classNames = [], attributes = {}, events = {} },
+  ...children
+) {
+  const element = document.createElement(tag);
+  if (classNames.length) {
+    element.classList.add(...classNames);
+  }
+  for (const [nameAttr, valueAttr] of Object.entries(attributes)) {
+    element.setAttribute(nameAttr, valueAttr);
+  }
+  for (const [typeEvent, handlerEvent] of Object.entries(events)) {
+    element.addEventListener(typeEvent, handlerEvent);
+  }
+  element.append(...children);
+  return element;
+}
+
+function createCard(actor) {
+  // const url = new URL(user.contacts[0]);
+  // const a = document.createElement('a');
+  // a.classList.add(mapSocialClass.get(url.hostname));
+  // console.log(a)
+  /*** ***/
+
+  const h2 = createElement(
+    "h2",
+    { classNames: ["card-fullname"] },
+    document.createTextNode(fullName(actor) || "Noname")
+  );
+  return createElement(
+    "article",
+    { classNames: ["card-container"] },
+    createWrapper(actor),
+    h2,
+    createLinks(actor)
+  );
+}
+
+function createLinks(actor) {
+  const { contacts } = actor;
+  const link = document.createElement("a");
+  link.classList.add("card-link-wrapper");
+  console.log(contacts[0]);
+  link.setAttribute("href", contacts[0]);
+  return link;
+}
+
+function createImage({ id, profilePicture, lastName }) {
+  const img = document.createElement("img");
+  img.classList.add("card-photo");
+  img.setAttribute("src", profilePicture);
+  img.setAttribute("alt", lastName);
+  img.dataset.id = `wrapper-${id}`;
+  img.addEventListener("error", photoErrorHandler);
+  img.addEventListener("load", photoLoadHandler);
+}
+
+function createWrapper(actor) {
+  const { id, lastName } = actor;
+  const photoWrapper = document.createElement("div");
+  photoWrapper.classList.add("card-photo-wrapper");
+  photoWrapper.setAttribute("id", `wrapper-${id}`);
+
+  const initials = document.createElement("div");
+  initials.classList.add("card-initials");
+  initials.style.backgroundColor = stringToColour(lastName);
+  initials.append(document.createTextNode(lastName[0] || "NN")); /* home work */
+
+  photoWrapper.append(initials);
+  createImage(actor);
+  return photoWrapper;
+}
+
+function photoLoadHandler({ target }) {
+  const parent = document.getElementById(target.dataset.id);
+  parent.append(target);
+}
+
+/* handler */
+function photoErrorHandler({ target }) {
+  target.remove();
+  return;
+}
+
+/* utilits */
+function stringToColour(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = "#";
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).slice(-2);
+  }
+  return colour;
+}
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((actors) => {
+    const cards = actors.map((actor) => createCard(actor));
+    root.append(...cards);
+  })
+  .catch((error) => {
+    document.body.prepend(document.createTextNode("500"));
+    if (error instanceof TypeError) {
+      console.error("Ошибка соединения: ", error);
+    } else if (error instanceof SyntaxError) {
+      console.error("Проверь запятые: ", error);
+    } else {
+      console.error(error);
+    }
+  });
